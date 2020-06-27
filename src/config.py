16f998cbd9e635b19c2b62d86463aea574cc65d3
SoCalDude <lesgainous@hotmail.com>
2020-06-27 00:05:35 -0800

    chart is a required folder (at least for now)

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ class ChartFocus(Enum):
     DEATHS = 3
     NEW_DEATHS = 4
 
-APP_VERSION: str = "1.00.002"
+APP_VERSION: str = "1.00.003"
 DATA_URL: str = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 LOCAL_DATA_FILE_NAME: str = r"data\covid19.csv"
 LOCAL_STATES_COUNTIES_FILE_NAME: str = r"data\states-counties.csv"
